d1066deffcda9e958ba9d4f9793927537d084d5c
Josh Delgadillo <delgadillo.i.joshua@gmail.com>
2021-08-09 23:39:43 -0700

    Did a suprising amount of work to make a fetch request function

--- client/src/views/Predict.jsx
+++ client/src/views/Predict.jsx
@@ -1,10 +1,29 @@
-import { Box, Heading, Stack } from "@chakra-ui/layout";
-import { FormControl, Input, Select, Text } from "@chakra-ui/react";
+import { Box, Heading, Stack, Button, Input, Select, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
 
 function Predict(props) {
 
+    /*const [athleteData, setAthleteData] = useState({}); // !in addition, SR, avg time that season/thus far, team ranking?, all time PR?
+    const [courseData, setCourseData] = useState({}); // !in addition, avg time drop/team time drop/num PRs after computation though in new dataset*/
+
+    const [athleteUrl, setAthleteUrl] = useState("");
+    const [meetUrl, setMeetUrl] = useState("");
     const [year, setYear] = useState("");
+
+    const onSelectAthlete = (e) => {
+        e.preventDefault();
+
+        // ! can make lib to automatically add content header?
+        fetch("/api/getAthleteData", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                athleteUrl: athleteUrl
+            })
+        }).then(res => res.json()).then(data => console.log(data)).catch((err) => console.error(err));
+    }
 
     return (
         <Box py="15vh">
@@ -14,7 +33,7 @@ function Predict(props) {
                 <Box>
                     <Box>
                         <Text>Athlete</Text>
-                        <Input placeholder="Paste the athlete's athletic.net url" type="text" />
+                        <Input placeholder="Paste the athlete's athletic.net url" type="text" value={athleteUrl} onChange={(e) => setAthleteUrl(e.target.value)}/>
                     </Box>
                     <Box>
                         <Text>Year</Text>
@@ -25,6 +44,16 @@ function Predict(props) {
                                 ))
                             }
                         </Select>
+                    </Box>
+                    <Button onClick={onSelectAthlete}>Select</Button>
+                </Box>
+                <Box>
+                    <Heading>Courses!</Heading>
+                    <Box>
+                        <Box>
+                            <Text>Course</Text>
+                            <Input placeholder="Paste the meet's athletic.net url" type="text" value={meetUrl} onChange={(e) => setMeetUrl(e.target.value)} />
+                        </Box>
                     </Box>
                 </Box>
             </Stack>
--- server/index.js
+++ server/index.js
@@ -4,8 +4,15 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Body parsing
+app.use(express.json());
+
 app.get("/api", (req, res) => {
     res.json({message: "Hello, world!"});
+})
+
+app.post("/api/getAthleteData", (req, res) => {
+    res.json({url: req.body.athleteUrl});
 })
 
 app.listen(PORT, () => {
